e5cb5a56bbf1f6c1b799ce20b262a2b3e75ccdaf
Nano <nanoticity@gmail.com>
2025-08-13 09:25:17 -0700

    Add Animal.java

---
+++ 08-13/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    public static void main(String[] args) {
+        System.out.println(" /\\_/\\  ");
+        System.out.println("( o.o ) ");
+        System.out.println(" > ^ <  ");
+        System.out.println("  / \\   ");
+        System.out.println(" (   )  ");
+    }
+}
