461e0cf0126d91944b7926184899d5aa9ae6a932
Nano <nanoticity@gmail.com>
2025-08-15 09:39:42 -0700

    Add requied characters, they were left out

--- 08-15/Landmark.java
+++ 08-15/Landmark.java
@@ -1,8 +1,9 @@
 public class Landmark {
     public static void main(String[] args) {
-        System.out.println("        ^");
+        System.out.println("        \"");
         System.out.println("        |");
         System.out.println("        |");
+        System.out.println("        '");
         System.out.println("       / \\");
         System.out.println("       | |");
         System.out.println("       | |");
