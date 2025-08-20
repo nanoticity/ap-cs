81c79b0e1eb7c09aebdf687922690b369de45657
Nano <nanoticity@gmail.com>
2025-08-20 08:29:08 -0700

    Commit work from 08/18

---
+++ 08-18/MathTest.java
@@ -0,0 +1,5 @@
+public class MathTest {
+    public static void main(String[] args) {
+	    int test = 1/0;
+    }
+}
---
+++ 08-18/Output.java
@@ -0,0 +1,9 @@
+public class Output {
+    public static void main(String[] args) {
+        System.out.println(" -------\n" + 
+        "|\t|\n" + 
+        "|\t|\n" + 
+        "|\t|\n" + 
+        " -------");
+    }
+}
